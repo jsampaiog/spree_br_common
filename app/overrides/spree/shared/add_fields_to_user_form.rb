d0dcffc11ffc512cfb0ebeaed27f60403f8c37af
josesampaio <josesampaiog@gmail.com>
2021-07-18 15:01:10 +0200

    rollback .deface files

--- app/overrides/spree/shared/add_fields_to_user_form.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_user_form',
-                     :name => 'add_fields_to_user_form',
-                     :insert_before => '[data-hook="signup_after_email_fields"]',
-                     :partial => 'spree/shared/add_extra_fields')
--- app/overrides/spree/users/add_details_to_users.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/users/show',
-                     :name => 'add_details_to_users',
-                     :insert_top => '#user-info',
-                     :partial => 'spree/users/add_details')
